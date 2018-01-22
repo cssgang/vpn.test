f6a98199045ad9b8bb0b5d7316cf53df1dee115a
Troy Wilson <troy@merchantguy.com>
2018-01-21 23:26:01 -0600

    Commented out lines to start beautiful soup

--- vpn.test.py
+++ vpn.test.py
@@ -9,12 +9,12 @@ opener = urllib2.build_opener(urllib2.HTTPCookieProcessor())
 oururl = opener.open('http://www.ip-lookup.net')
 
 ## IP Addresss finder ##
-theIP = re.compile(r"d{1,3}.d{1,3}.d{1,3}.d{1,3}")
-ip = re.search(theIP, str(oururl))
+#theIP = re.compile(r"d{1,3}.d{1,3}.d{1,3}.d{1,3}")
+#ip = re.search(theIP, str(oururl))
 
 ## Country finder ##
-roughCountry = re.compile('([A-Z]\w+)( [A-Z]\w+){0,2}(?=\<\/a\>\s\s)')
-Country = re.search(roughCountry, str(oururl))
+#roughCountry = re.compile('([A-Z]\w+)( [A-Z]\w+){0,2}(?=\<\/a\>\s\s)')
+#Country = re.search(roughCountry, str(oururl))
 
 ## Print out ##
 print "Your IP is:", ip
