4c5a9c60f89cfb7f93255a4c628a09bf390761c7
Troy Wilson <troy@merchantguy.com>
2018-01-22 23:47:05 -0600

    Fixing Web pull

--- vpn.test.py
+++ vpn.test.py
@@ -5,20 +5,21 @@ import re
 
 ## Open Connection ##
 opener = urllib2.build_opener(urllib2.HTTPCookieProcessor())
-oururl = opener.open('http://www.ip-lookup.net')
+opener.addheaders = [('User-agent', 'Mozilla/5.0')]
+website = urllib2.urlopen('http://www.example.com/')
+website_html = website.read()
 
 ## Output of oururl to file ##
-text_file = open("Output.txt", "w")
-text_file.write(oururl)
-text_file.close()
+with open("Output.txt", "w") as text_file:
+    text_file.write("Purchase Amount: %s" % website_html)
 
 ## IP Addresss finder ##
-theIP = re.compile(r"\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}")
-ip = re.search(theIP, str(oururl))
+#theIP = re.compile(r"\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}")
+#ip = re.search(theIP, str(html))
 
 ## Country finder ##
-roughCountry = re.compile('([A-Z]\w+)( [A-Z]\w+){0,2}(?=\<\/a\>\s\s)')
-Country = re.search(roughCountry, str(oururl))
+#roughCountry = re.compile('([A-Z]\w+)( [A-Z]\w+){0,2}(?=\<\/a\>\s\s)')
+#Country = re.search(roughCountry, str(html))
 
 ## Print out ##
 print "Your IP is:", ip
