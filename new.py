ef3280a253c2bf9002ffc5aef05aea6bd558ca00
Troy Wilson <troy@merchantguy.com>
2018-01-23 12:16:15 -0600

    Fixed IP Address Fetch

--- new.py
+++ new.py
@@ -1,12 +1,18 @@
 #!/usr/bin/python
 #
-from json import load
-from urllib2 import urlopen
+import urllib2
 import requests
+import re
 
 ## Get IP ##
-my_ip = urlopen('http://www.ipchicken.com/')
-print my_ip
+opener = urllib2.build_opener()
+opener.addheaders = [('User-agent', 'Mozilla/5.0')]
+my_ip = opener.open('http://ipchickenhawk.com/')
+my_ip =  my_ip.read()
+iplist = re.findall( r'[0-9]+(?:\.[0-9]+){3}', my_ip ) [0]
+
+
+print iplist
 
 ## Get Geolocation ##
 #geo = requests.get('http://ipinfo.io/%s/country' % my_ip)
