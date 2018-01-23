c6aa37269600d9cfc784af7c06c547b90c443b10
Troy Wilson <troy@merchantguy.com>
2018-01-23 09:10:46 -0600

    Use both libs

--- vpn.test.py
+++ vpn.test.py
@@ -1,17 +1,27 @@
 #!/usr/bin/python
 #
+import urllib
 import urllib2
 import re
 
 ## Open Connection ##
-opener = urllib2.build_opener(urllib2.HTTPCookieProcessor())
-opener.addheaders = [('User-agent', 'Mozilla/5.0')]
-website = urllib2.urlopen('http://www.ip-lookup.net')
-website_html = website.read()
+url = 'http://www.ip-look up.net'
+user_agent = 'Mozilla/5.0'
+headers = {'User-Agent':user_agent}
+
+req = urllib2.Request(url, headers)
+response = urllib2.urlopen(req)
+the_page = response.read()
+
+
+#opener = urllib2.build_opener(urllib2.HTTPCookieProcessor())
+#opener.addheaders = [('User-agent', 'Mozilla/5.0')]
+#website = urllib2.urlopen('http://www.ip-lookup.net')
+#website_html = website.read()
 
 ## Output of oururl to file ##
 with open("Output.txt", "w") as text_file:
-    text_file.write(website_html)
+    text_file.write(the_page)
 
 ## IP Addresss finder ##
 #theIP = re.compile(r"\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}")
