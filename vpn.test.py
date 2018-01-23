ec177719109ddf8a21210e00a923c5d84d8efa7e
Troy Wilson <troy@merchantguy.com>
2018-01-22 23:09:47 -0600

    Added file log to test output

--- vpn.test.py
+++ vpn.test.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python
 #
-from bs4 import BeautifulSoup
 import urllib2
 import re
 
@@ -8,18 +7,18 @@ import re
 opener = urllib2.build_opener(urllib2.HTTPCookieProcessor())
 oururl = opener.open('http://www.ip-lookup.net')
 
-soup = BeautifulSoup(oururl)
-print soup.prettify()
-print 
-<span class="vbottom monospace">
+## Output of oururl to file ##
+text_file = open("Output.txt", "w")
+text_file.write(oururl)
+text_file.close()
 
 ## IP Addresss finder ##
-#theIP = re.compile(r"d{1,3}.d{1,3}.d{1,3}.d{1,3}")
-#ip = re.search(theIP, str(oururl))
+theIP = re.compile(r"\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}")
+ip = re.search(theIP, str(oururl))
 
 ## Country finder ##
-#roughCountry = re.compile('([A-Z]\w+)( [A-Z]\w+){0,2}(?=\<\/a\>\s\s)')
-#Country = re.search(roughCountry, str(oururl))
+roughCountry = re.compile('([A-Z]\w+)( [A-Z]\w+){0,2}(?=\<\/a\>\s\s)')
+Country = re.search(roughCountry, str(oururl))
 
 ## Print out ##
 print "Your IP is:", ip
